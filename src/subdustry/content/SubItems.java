6b6276995246323cea5a83fed9b6db65290c3e7a
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2024-08-09 00:54:32 +0300

    Added more items

--- src/subdustry/content/SubItems.java
+++ src/subdustry/content/SubItems.java
@@ -5,9 +5,13 @@ import mindustry.type.*;
 
 public class SubItems {
     public static Item
-            titaniumOre, copperOre, quartz;
+            metalSalvage, titaniumOre, copperOre, quartz, caveSulfur, acidMushroom, glass;
 
     public static void load() {
+        metalSalvage = new Item("metal-salvage", Color.valueOf("#cbcadb")) {{
+            hardness = 1;
+        }};
+
         titaniumOre = new Item("titanium", Color.valueOf("#919fe7")) {{
             hardness = 2;
             cost = 0.5f;
@@ -21,5 +25,17 @@ public class SubItems {
         quartz = new Item("quartz", Color.valueOf("#dfe8ef")) {{
             hardness = 2;
         }};
+
+        caveSulfur = new Item("cave-sulfur", Color.valueOf("#d0ae1f")){{
+            flammability = 1;
+            explosiveness = 0.6f;
+        }};
+
+        acidMushroom = new Item("acid-mushroom", Color.valueOf("#f3c4e7")){{
+            flammability = 0.3f;
+            charge = 0.1f;
+        }};
+
+        glass = new Item("glass", Color.valueOf("#65b7ff"));
     }
 }
