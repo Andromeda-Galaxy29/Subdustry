bcf866950c07feac39c28a6031e2de63ae2d40cd
Haznobrain <143508779+Haznobrain@users.noreply.github.com>
2024-08-12 10:42:27 +0200

    ;

--- src/subdustry/content/SubSounds.java
+++ src/subdustry/content/SubSounds.java
@@ -6,6 +6,6 @@ import mindustry.Vars;
 public class SubSounds{
     public static Sound muffledExplosion;
     public static void load(){
-        muffledExplosion = Vars.tree.loadSound("muffledExplosion")
+        muffledExplosion = Vars.tree.loadSound("muffledExplosion");
             }
 }
